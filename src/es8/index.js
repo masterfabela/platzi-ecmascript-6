7ea23f199b9ddcc975da8c2156bfc16d826fa5e6
Jose Ramon Vieites <jose.vieites@altia.es>
2021-05-09 15:07:19 +0200

    ES8

--- src/es8/index.js
+++ src/es8/index.js
@@ -20,6 +20,25 @@ console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, '-----'));
 console.log('food'.padEnd(12, '-'));
 
-const obj = {
-  name: 'oscar',
+//Asinc await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    false
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Es error'));
+  });
 };
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+helloAsync();
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+anotherFunction();
